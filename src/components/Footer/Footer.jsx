5ed6330af8982b6a9546c0598ad5561adad6585a
Gorschy <54874863+Gorschy@users.noreply.github.com>
2023-11-17 22:36:28 +1100

    test

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css'
+function Footer() {
+    return(
+        <div className='footer'>
+            <div class="copyright">
+                <p>© 2023 NathanG, Inc.</p>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
