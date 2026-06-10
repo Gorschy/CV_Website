fe9d4295aa7872b74a3d14afca135f2d19766405
Nathan <minigorsch@live.com>
2026-06-10 16:21:16 +1000

    updated import
    updated how import for image carousel works making it more dynamic. addjusted some a tags to strong as no link to be provided.

--- src/components/About/Qualifications.jsx
+++ src/components/About/Qualifications.jsx
@@ -1,45 +1,28 @@
 import React from 'react';
 import Carousel from "./carousel/Carousel";
-import "./Qualifications.css"
-import Bachelors from './Bachelors.png';
+import "./Qualifications.css";
 
-import pic1 from "./carousel/assets/1.png";
-import pic2 from "./carousel/assets/2.png";
-import pic3 from "./carousel/assets/3.png";
-import pic4 from "./carousel/assets/4.png";
-import pic5 from "./carousel/assets/5.png";
-import pic6 from "./carousel/assets/6.png";
-import pic7 from "./carousel/assets/7.png";
-import pic8 from "./carousel/assets/8.png";
+const slides = [1, 2, 3, 4, 5, 6, 7, 8].map(
+  (n) => require(`./carousel/assets/${n}.png`)
+);
 
 const Qualifications = () => {
-
-  const slides = [
-    pic1,
-    pic2,
-    pic3,
-    pic4,
-    pic5,
-    pic6,
-    pic7,
-    pic8,
-  ]
-
   return (
-      <div className='qualification-container'>
-        <h3 className='heading'><span className='highlight'>02. </span> My Qualifications</h3>
-        
-        <p className='qualification-text'>
-          I've recieved a <a>Bachelors of Computer Science</a> majoring in Software Engineering awarded to me by the University of Wollongong(UOW).
-          <br />
-          <br />
-          Additionally I've furthered my education through multiple LinkedIn courses, my certificates can be found below.
-        </p>
-        
-        
-        <h3>LinkedIn Certifications</h3>
-        <Carousel slides={slides} controls indicators interval={6000} autoPlay={true}/>
-      </div>
+    <div className='qualification-container'>
+      <h3 className='heading'><span className='highlight'>02. </span> My Qualifications</h3>
+      
+      <p className='qualification-text'>
+        I've received a <strong>Bachelors of Computer Science</strong> majoring in Software Engineering 
+        awarded to me by the University of Wollongong (UOW).
+        <br />
+        <br />
+        Additionally I've furthered my education through multiple LinkedIn courses, 
+        my certificates can be found below.
+      </p>
+
+      <h3>LinkedIn Certifications</h3>
+      <Carousel slides={slides} controls indicators interval={6000} autoPlay={true} />
+    </div>
   );
 };
 
